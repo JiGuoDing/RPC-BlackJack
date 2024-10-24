b7776da0b3241a4bc2b10d642e8472d311b801b4
JiGuoDing <485204300@qq.com>
2024-10-24 10:54:53 +0800

    status from ptr 2 obj

--- src/c/blackJackClient.c
+++ src/c/blackJackClient.c
@@ -33,46 +33,52 @@ int main(int argc, char* argv[])
     free(ready);
 
     // rpc startGame 开始游戏
-    // GameStatus status;
-    GameStatus* status = startgame_1(NULL, clnt);
+    GameStatus* statusPtr = (GameStatus*)calloc(1, sizeof(GameStatus));
     // 游戏初始化失败
-    if (status == NULL)
+    if (statusPtr == NULL) {
         clnt_perror(clnt, "game starting failed");
+        free(clnt);
+        exit(10086);
+    }
+    statusPtr = startgame_1(NULL, clnt);
+    // 游戏状态变量
+    GameStatus status = *statusPtr;
 
     printf("\n**********game starts**********\n");
-    printf("\nDealer's first card is %s\n", status->dealer.cards.cards_val[0].face);
-    printf("\nPlayer's first card is %s\n", status->player.cards.cards_val[0].face);
-    printf("\nPlayer's second card is %s\n", status->player.cards.cards_val[1].face);
+    printf("\nDealer's first card is %s\n", status.dealer.cards.cards_val[0].face);
+    printf("\nPlayer's first card is %s\n", status.player.cards.cards_val[0].face);
+    printf("\nPlayer's second card is %s\n", status.player.cards.cards_val[1].face);
 
     // 定义一个发牌请求（包括游戏状态和玩家id）
     HitRequest hreq;
-    hreq.gameStatus = status;
+    hreq.gameStatus = &status;
     hreq.id = 1;
+    char* choice = (char*)calloc(10, sizeof(char));
     printf("\nNow it's your turn!\n");
     while (TRUE) {
         // 玩家回合
-        char* choice = (char*)calloc(10, sizeof(char));
         printf("\nPlease choose to hit one more card or stand(h/s): ");
         scanf("%s", choice);
         // printf(status->msg);
         // 选择再发一张牌
         if (strcmp(_2Lower(choice), "h") == 0) {
-            status = hitonecard_1(&hreq, clnt);
-            if (status == NULL) {
+            statusPtr = hitonecard_1(&hreq, clnt);
+            if (statusPtr == NULL) {
                 printf("\nSERVER ERROR! failed to hit new card.\n");
                 free(choice);
                 break;
             }
-            printf("\nyour new card is: %s\n", status->player.cards.cards_val[status->player.cards.cards_len - 1].face);
-            printf("\nNow your current points is: %d\n", status->currentPointsOfPlayer);
+            status = *statusPtr;
+            printf("\nyour new card is: %s\n", status.player.cards.cards_val[status.player.cards.cards_len - 1].face);
+            printf("\nNow your current points is: %d\n", status.currentPointsOfPlayer);
         } else if (strcmp(_2Lower(choice), "s") == 0) {
             free(choice);
             break;
-        } else
-            free(choice);
+        }
+        // printf("\n%d\n", status.player.cards.cards_len);
         // 如果手牌数 >= 20，强制进入庄家回合
-        printf("\n%d\n", status->player.cards.cards_len);
-        if (status->player.cards.cards_len >= 20) {
+        if (status.player.cards.cards_len >= 20) {
+            free(choice);
             printf("\nYour cards have reached the limit number!\n");
             break;
         }
@@ -80,27 +86,29 @@ int main(int argc, char* argv[])
 
     printf("\nYour turn is over!\n");
     printf("\nNow it's dealer's turn!\n");
-    printf("\ndealer's another card is: %s\n", status->dealer.cards.cards_val[status->dealer.cards.cards_len - 1].face);
-    hreq.gameStatus = status;
+    printf("\ndealer's another card is: %s\n", status.dealer.cards.cards_val[status.dealer.cards.cards_len - 1].face);
+    hreq.gameStatus = &status;
     hreq.id = 0;
     while (TRUE) {
         // 庄家回合
         // 手牌点数小于17，强制发牌
-        if (status->currentPointsOfDealer < 17) {
-            status = hitonecard_1(&hreq, clnt);
-            printf("\ndealer hit a new card: %s\n", status->dealer.cards.cards_val[status->dealer.cards.cards_len - 1].face);
+        if (status.currentPointsOfDealer < 17) {
+            statusPtr = hitonecard_1(&hreq, clnt);
+            if (statusPtr == NULL) {
+                printf("\nSERVER ERROR! failed to hit new card.\n");
+                break;
+            }
+            status = *statusPtr;
+            printf("\ndealer hit a new card: %s\n", status.dealer.cards.cards_val[status.dealer.cards.cards_len - 1].face);
         } else {
             // TODO 庄家的逻辑
             // break;
         }
-        if (status == NULL) {
-            printf("\nSERVER ERROR! failed to hit new card.\n");
-            break;
-        }
     }
 
-    // 释放客户端内存
+    // 释放分配的内存
     free(clnt);
+    free(statusPtr);
     printf("\n**********Game Over**********\n");
 
     return 0;
